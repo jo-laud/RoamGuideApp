dccbf079406bad6337ede8e1ed5e1dba5118d14f
jo-laud <lawlesslaw90@gmail.com>
2023-11-14 14:47:38 +0000

    Initial commit

---
+++ app/src/main/java/com/example/guide2/intro.java
@@ -0,0 +1,42 @@
+package com.example.guide2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class intro extends AppCompatActivity {
+
+    private static int Splash=5000;
+
+    ImageView logo;
+    LottieAnimationView lottieAnimationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        logo=findViewById(R.id.whitelo);
+        lottieAnimationView=findViewById(R.id.lottie);
+
+
+        logo.animate().translationY(-1400).setDuration(4000).setStartDelay(180);
+        lottieAnimationView.animate().translationY(1400).setDuration(4000).setStartDelay(200);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(intro.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },Splash);
+
+    }
+}
